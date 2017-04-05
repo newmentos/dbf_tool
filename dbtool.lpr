be038fcda6e4215687b44b7907f372caebebfaaa
newmentos <newmentos@gmail.com>
2017-04-05 17:26:30 +0300

    first commit

---
+++ dbtool.lpr
@@ -0,0 +1,21 @@
+program dbtool;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, dbflaz, main
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
+
---
+++ main.pas
@@ -0,0 +1,76 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, dbf, FileUtil, Forms, Controls, Graphics, Dialogs,
+  StdCtrls, DBGrids;
+
+type
+
+  { TfrmMain }
+
+  TfrmMain = class(TForm)
+    btnOpen: TButton;
+    btnExit: TButton;
+    btnSave: TButton;
+    dbf1: TDbf;
+    dbgrid1: TDBGrid;
+    memoLog: TMemo;
+    OpenDialog1: TOpenDialog;
+    procedure btnExitClick(Sender: TObject);
+    procedure btnOpenClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+  curDir: string;
+  filename: string;
+
+implementation
+
+{$R *.lfm}
+
+{ TfrmMain }
+
+procedure TfrmMain.btnExitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TfrmMain.btnOpenClick(Sender: TObject);
+var
+  i: integer;
+begin
+  if OpenDialog1.Execute then
+  begin
+    filename := OpenDialog1.Filename;
+  end;
+  memoLog.Append(filename);
+  dbf1.FilePathFull := ExtractFilePath(filename);
+  dbf1.TableName := ExtractFileName(filename);
+  dbf1.Active := True;
+  dbf1.Open;
+  for i := 0 to dbf1.FieldCount - 1 do
+  begin
+    // dbf1.Fields[i].DataType = ftString ftFloat ftInteger
+    memoLog.Append(dbf1.Fields[i].FieldName+' '+IntToStr(dbf1.Fields[i].DataSize));
+  end;
+  dbf1.Close;
+end;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+  curDir := ExtractFilePath(Application.ExeName);
+  OpenDialog1.Filter := 'DBase Table|*.dbf*;.DBF';
+  memoLog.Clear;
+end;
+
+end.
